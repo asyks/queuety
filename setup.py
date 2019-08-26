bbabf25aea49ffa30a17ec27f880fc5878cb9ee8
asyks <aaron@aaronsykes.com>
2019-08-26 00:11:03 -0700

    Remove deprecated setup setup_cfg arg

--- setup.py
+++ setup.py
@@ -6,4 +6,4 @@ from setuptools import setup
 with open("requirements.txt") as req_file:
     REQUIREMENTS = [l.strip() for l in req_file.readlines()]
 
-setup(install_requires=REQUIREMENTS, setup_cfg=True)
+setup(install_requires=REQUIREMENTS)

6648a0a694a4d07ea934594e534b912309128d31
asyks <aaron@aaronsykes.com>
2019-08-26 00:06:54 -0700

    Refactor yet again to avoid runtime warning

--- queuety/__main__.py
+++ queuety/__main__.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
 
 import asyncio
-import queue
 
-from . import log, pub, sub
-
-
-q = queue.Queue(100)
+from . import log, entry
 
 
-async def main(n: int = 10) -> None:
-    pub.enqueue(q, n)
-    await asyncio.gather(
-        *[sub.handle_dequeued(dequeued) for dequeued in sub.dequeue(q)]
-    )
-
-
-if __name__ == "__main__":
-    log.setup()
-    asyncio.run(main())
+log.setup()
+asyncio.run(entry.main())
--- tests/test_sub.py
+++ tests/test_sub.py
@@ -11,12 +11,12 @@ class TestSubscriber(TestCase):
         pass
 
     def test_sub_default_msg_num(self, mock_handle_dequeued):
-        asyncio.run(queuety.main())
+        asyncio.run(queuety.entry.main())
 
         self.assertEqual(mock_handle_dequeued.call_count, 10)
 
     def test_sub_custom_msg_num(self, mock_handle_dequeued):
         num_messages = 8
-        asyncio.run(queuety.main(num_messages))
+        asyncio.run(queuety.entry.main(num_messages))
 
         self.assertEqual(mock_handle_dequeued.call_count, num_messages)

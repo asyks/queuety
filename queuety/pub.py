1ce5307a233b273360ac7d3defb322a6ba3c2ce1
asyks <aaron@aaronsykes.com>
2019-08-25 23:29:05 -0700

    Improve pub.enqueue type signature

--- queuety/pub.py
+++ queuety/pub.py
@@ -1,10 +1,11 @@
 import logging
+import queue
 
 
 logger = logging.getLogger(__name__)
 
 
-def enqueue(q, n: int) -> None:
+def enqueue(q: queue.Queue, n: int) -> None:
     for _i in range(n):
         msg = f"message {_i}"
         q.put(msg)

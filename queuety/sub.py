a613a804c23e6caa1bb368833bc6fcda0eca70b7
asyks <aaron@aaronsykes.com>
2019-10-21 16:28:18 -0700

    Move extend and acknowledge functions into class scope

--- queuety/sub.py
+++ queuety/sub.py
@@ -11,20 +11,6 @@ from . import constants
 logger = logging.getLogger(__name__)
 
 
-async def extend_until_complete(msg: Message, event: asyncio.Event) -> t.Coroutine:
-    while not event.is_set():
-        msg.extend_count += 1
-        logging.info("Extended deadline for %s", msg.id)
-        await asyncio.sleep(constants.MESSAGE_EXTEND_DELAY)
-
-
-async def ack_dequeued_msg(msg: Message, event: asyncio.Event) -> t.Coroutine:
-    await event.wait()
-    await asyncio.sleep(random.randint(0, 1))
-    msg.acked = True
-    logger.info("Acknowledged %s", msg.id)
-
-
 class BaseSubsriber:
 
     def __init__(self, queue: asyncio.Queue) -> None:
@@ -33,13 +19,22 @@ class BaseSubsriber:
 
         super().__init__()
 
+    async def extend(self, msg: Message, event: asyncio.Event) -> t.Coroutine:
+        while not event.is_set():
+            msg.extend_count += 1
+            logging.info("Extended deadline for %s", msg.id)
+            await asyncio.sleep(constants.MESSAGE_EXTEND_DELAY)
+
+    async def acknowledge(self, msg: Message, event: asyncio.Event) -> t.Coroutine:
+        raise NotImplementedError
+
     async def handle_route(self, route: str, msg: Message) -> t.Coroutine:
         raise NotImplementedError
 
     async def handle_message(self, msg: Message) -> t.Coroutine:
         event = asyncio.Event()
-        asyncio.create_task(extend_until_complete(msg, event))
-        asyncio.create_task(ack_dequeued_msg(msg, event))
+        asyncio.create_task(self.extend(msg, event))
+        asyncio.create_task(self.acknowledge(msg, event))
 
         results = await asyncio.gather(
             *(
@@ -61,12 +56,19 @@ class BaseSubsriber:
 
 
 class SimulatedSubscriber(BaseSubsriber):
+    """Extend BaseSubscriber: Implement coroutines which simulate IO intensive calls.
 
+    Suspend each coroutine for a random, and realistic, but short amount of time.
+    """
     async def handle_route(self, route: str, msg: Message) -> t.Coroutine:
-        """Simulate route handling for a message.
-
-        Suspend the route for a random, realistic, but short amount of time.
-        """
+        """Simulate route handling for a message."""
         await asyncio.sleep(random.random())
         msg.routes[route] = True
         logger.info("Handled route %s for %s", route, msg.id)
+
+    async def acknowledge(self, msg: Message, event: asyncio.Event) -> t.Coroutine:
+        """Simulate messsage acknowledgement."""
+        await event.wait()
+        await asyncio.sleep(random.random())
+        msg.acked = True
+        logger.info("Acknowledged %s", msg.id)
